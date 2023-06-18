45131732d1c86ee31e4606f7bd23df083b4eaf95
KeyonYan <835254227@qq.com>
2023-06-18 23:54:14 +0800

    ✨ feat(Front): 主页布局
    布局（Header+Content+Footer），加入骨架屏

--- SFileHub-Front/src/pages/home/homePage.tsx
+++ SFileHub-Front/src/pages/home/homePage.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import NavBar from '../../components/navBar/navBar'
+import { Breadcrumb, Layout, Menu, theme, Skeleton } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 const HomePage: React.FC = () => {
   return (
-    <NavBar/>
+    <Layout className="layout">
+      <Header className='bg-white' style={{ display: 'flex', alignItems: 'center' }}>
+        <NavBar/>
+      </Header>
+      <Content className="p-10 bg-white space-y-10">
+        <Skeleton active />
+        <Skeleton active />
+        <Skeleton active />
+        <Skeleton active />
+        <Skeleton active />
+        <Skeleton active />
+      </Content>
+      <Footer className='bottom-0 left-0 right-0' style={{ textAlign: 'center' }}>SFileHub ©2023 Created by Keyon</Footer>
+    </Layout>
   )
 }
 export default HomePage;
