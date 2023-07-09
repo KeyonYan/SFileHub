af75ac9af7fe64e05abb1abcd18e66ba4676e6aa
KeyonYan <835254227@qq.com>
2023-07-10 00:43:36 +0800

    Update vite.config.ts

--- SFileHub-Front/vite.config.ts
+++ SFileHub-Front/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  base: './'
   resolve: {
     alias: {
       "/@": path.resolve(__dirname, "./src"),
