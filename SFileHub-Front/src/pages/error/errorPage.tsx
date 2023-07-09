59bf755042a163cb0de3194b2983143385220efe
KeyonYan <835254227@qq.com>
2023-07-09 22:39:22 +0800

    🐞 fix: test node build

--- SFileHub-Front/src/pages/error/errorPage.tsx
+++ SFileHub-Front/src/pages/error/errorPage.tsx
@@ -7,7 +7,7 @@ export default function ErrorPage() {
     // console.error(error);
     return (
         <div className="error-page grid place-content-center h-full">
-          <div className='text-3xl'>Oops! That page couldn't be found</div>
+          <div className='text-3xl'>Oops! That page couldn't be found </div>
         </div>
     );
 }
