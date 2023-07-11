6eb0e113e5c0268b08adfee35ae55a3d2b0341b8
KeyonYan <835254227@qq.com>
2023-07-11 22:18:19 +0800

    🦄 refactor(重构出ServiceWithRetry):

--- SFileHub-Front/src/pages/fileDownload/FileDownload.tsx
+++ SFileHub-Front/src/pages/fileDownload/FileDownload.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { queryFileList } from "@/api";
+import { message } from 'antd';
+
+interface FileType {
+    identifier: string,
+    fileName: string,
+    suffix: string,
+    fileType: string,
+    size: number,
+    createBy: string,
+}
 
 const FileDownload: React.FC = () => {
+    const [messageApi] = message.useMessage();
+    const [fileList, setFileList] = useState<FileType[]>([]); 
+    
+    useEffect(() => {
+        queryFileList()
+            .then(res => {
+                if (res.code != 0) {
+                    messageApi.open({
+                        type: 'error',
+                        content: res.msg,
+                    });
+                }
+                setFileList(res.data);
+                console.log(fileList);
+            });
+    }, []);
+
+    const listItems = fileList.map(file =>
+        <li key={file.identifier}>
+            {file.fileName}
+        </li>
+    );
     return (
         <>
             <h1 className="flex justify-center text-xl font-semibold leading-6 text-gray-900">
                 🗃️ FileDownloadPage
             </h1>
+            <ul>{listItems}</ul>
         </>
     )
 }
