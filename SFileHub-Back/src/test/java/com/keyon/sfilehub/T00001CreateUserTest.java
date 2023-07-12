cb58d174bd6b7ca51c34e66b5e101e7328e6fdf8
KeyonYan <835254227@qq.com>
2023-07-12 21:24:33 +0800

    🧪test: 集成测试 (#5)
    * Merge (#3)
    
    * Dev (#1)
    
    * 📃 docs: Title居中
    
    * 📃 docs: del line
    
    * Update maven.yml
    
    * Update node.yml
    
    * 🧪 test(Back): import WebTestClient
    
    * 🐳 chore(Back): WebClientTest from webflux
    
    * 🧪 test(Back): UnitTest
    
    HelloController
    
    * 🧪 test(Back): add TestClassOrder
    
    * 🧪 test(Back): fix TestClassOrder
    
    * 🧪 test(Back): fix Test Order
    
    * 🐞 fix(Back): not exec Test class
    
    * 🧪 test(Back): Assert&cookies
    
    * 🧪 test(Back): Request set cookie
    
    * 🐞 fix(Back): Fix cookie key
    
    * 🐞 fix(Back): set cookie static
    
    * 🧪 test(Back): finish IT
    
    * 🧪 test(Back): 重构测试代码
    
    * 📃 docs: 追加Features
    
    * 🐎 ci: 测试类执行顺序：字母

--- SFileHub-Back/src/test/java/com/keyon/sfilehub/T00001CreateUserTest.java
+++ SFileHub-Back/src/test/java/com/keyon/sfilehub/T00001CreateUserTest.java
@@ -4,13 +4,10 @@ import com.keyon.sfilehub.dao.RoleDao;
 import com.keyon.sfilehub.dao.UserDao;
 import com.keyon.sfilehub.entity.Role;
 import com.keyon.sfilehub.entity.User;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.HashSet;
@@ -18,7 +15,7 @@ import java.util.Set;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class CreateUserTest {
+public class T00001CreateUserTest {
 
     @Autowired
     private UserDao userDao;
@@ -32,14 +29,19 @@ public class CreateUserTest {
     @Test
     @Order(1)
     public void addRole() {
+        System.out.println("11111");
+        if (roleDao.findByTitle("ROLE_ADMIN") != null) return;
         Role role = Role.builder().title("ROLE_ADMIN").label("管理员").intro("最高权限者").enable(true).build();
         roleDao.save(role);
-        role = Role.builder().title("ROLE_USER").label("用户").intro("普通用户").enable(true).build();
-        roleDao.save(role);
+        if (roleDao.findByTitle("ROLE_USER") != null) return;
+        Role role2 = Role.builder().title("ROLE_USER").label("用户").intro("普通用户").enable(true).build();
+        roleDao.save(role2);
     }
 
     @Test
+    @Order(2)
     public void addAdmin() {
+        if (userDao.findByUsername("admin") != null) return;
         User user = User.builder()
                 .username("admin")
                 .password(passwordEncoder.encode("123456"))
@@ -58,7 +60,9 @@ public class CreateUserTest {
     }
 
     @Test
+    @Order(2)
     public void addUser() {
+        if (userDao.findByUsername("user") != null) return;
         User user = User.builder()
                 .username("user")
                 .password(passwordEncoder.encode("123456"))
